8966da9d51a4189afa21ffab4e08e5ca539fbad7
slowey <truonglevinhphuc2006@gmail.com>
2024-07-19 01:50:26 +0700

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -225,7 +225,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
 
                         <Button
                             onClick={() =>
-                                window.open("mailto:hello@chetanverma.com")
+                                window.open("mailto:sloweycontact@gmail.com")
                             }
                         >
                             Contact
