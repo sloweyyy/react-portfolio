30047e4b08c2b6d593f65b07291932513c286754
slowey <truonglevinhphuc2006@gmail.com>
2024-07-25 02:22:23 +0700

    chore: Update dependencies, add vercel analytics,

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }) => {
     return (
