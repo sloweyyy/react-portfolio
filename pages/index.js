969fa72e2b9f2a436c7daca40a7fdb6bf304be8f
slowey <truonglevinhphuc2006@gmail.com>
2024-07-28 10:49:18 +0700

    chore: Update fonts and styles for better readability and consistency

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
     // Handling Scroll
     const handleWorkScroll = () => {
         window.scrollTo({
-            top: workRef.current.offsetTop,
+            top: workRef.current.offsetTop - 25,
             left: 0,
             behavior: "smooth",
         });
@@ -34,7 +34,7 @@ export default function Home() {
 
     const handleAboutScroll = () => {
         window.scrollTo({
-            top: aboutRef.current.offsetTop,
+            top: aboutRef.current.offsetTop - 25,
             left: 0,
             behavior: "smooth",
         });
@@ -86,25 +86,25 @@ export default function Home() {
                     <div className="mt-5">
                         <h1
                             ref={textOne}
-                            className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-4/5 mob:w-full laptop:w-4/5"
+                            className="text-3xl tablet:text-5xl laptop:text-6xl laptopl:text-7xl p-1 tablet:p-2 w-4/5 mob:w-full laptop:w-4/5"
                         >
                             {data.headerTaglineOne}
                         </h1>
                         <h1
                             ref={textTwo}
-                            className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                            className="text-3xl tablet:text-5xl laptop:text-6xl laptopl:text-7xl p-1 tablet:p-2 w-full laptop:w-4/5"
                         >
                             {data.headerTaglineTwo}
                         </h1>
                         <h1
                             ref={textThree}
-                            className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                            className="text-3xl tablet:text-5xl laptop:text-6xl laptopl:text-7xl p-1 tablet:p-2 w-full laptop:w-4/5"
                         >
                             {data.headerTaglineThree}
                         </h1>
                         <h1
                             ref={textFour}
-                            className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
+                            className="text-3xl tablet:text-5xl laptop:text-6xl laptopl:text-7xl p-1 tablet:p-2 w-full laptop:w-4/5"
                         >
                             {data.headerTaglineFour}
                         </h1>
@@ -112,13 +112,14 @@ export default function Home() {
 
                     <Socials className="mt-2 laptop:mt-5" />
                 </div>
+
                 <div
                     className="mt-10 laptop:mt-30 p-2 laptop:p-0"
                     ref={workRef}
                 >
                     <h1 className="text-2xl text-bold">Work.</h1>
 
-                    <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-4">
+                    <div className="mt-5 laptop:mt-10 grid grid-cols-1 tablet:grid-cols-2 gap-5">
                         {data.projects.map((project) => (
                             <WorkCard
                                 key={project.id}
@@ -132,19 +133,20 @@ export default function Home() {
                 </div>
 
                 <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-                    <h1 className="tablet:m-10 text-2xl text-bold">
-                        Services.
-                    </h1>
-                    <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
-                        {data.services.map((service, index) => (
-                            <ServiceCard
-                                key={index}
-                                name={service.title}
-                                description={service.description}
-                            />
-                        ))}
+                    <div className="ml-2 laptop:ml-0">
+                        <h1 className="text-2xl mb-2">Services.</h1>
+                        <div className="mt-5 grid grid-cols-1 laptop:grid-cols-2 gap-6">
+                            {data.services.map((service, index) => (
+                                <ServiceCard
+                                    key={index}
+                                    name={service.title}
+                                    description={service.description}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
+
                 {/* This button should not go into production */}
                 {process.env.NODE_ENV === "development" && (
                     <div className="fixed bottom-5 right-5">
@@ -157,11 +159,14 @@ export default function Home() {
                     className="mt-10 laptop:mt-40 p-2 laptop:p-0"
                     ref={aboutRef}
                 >
-                    <h1 className="tablet:m-10 text-2xl text-bold">About.</h1>
-                    <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
-                        {data.aboutpara}
-                    </p>
+                    <div className="mx-2 laptop:mx-0 laptop:w-3/5">
+                        <h1 className="text-2xl mb-2">About.</h1>
+                        <p className="text-lg laptop:text-2xl font-light">
+                            {data.aboutpara}
+                        </p>
+                    </div>
                 </div>
+
                 <Footer />
             </div>
         </div>
